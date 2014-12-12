f53cc6e1d4835236f019bb0cf25dc8af85dcb6d7
王佳 <wangjia@cvte.cn>
2014-12-12 10:09:14 +0800

    add slide back activity demo A

--- app/src/main/java/com/example/fragment/ActivityMainActivity.java
+++ app/src/main/java/com/example/fragment/ActivityMainActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.view.View;
 
 import com.example.fragment.pagerAndhost.ViewPagerAndHostActivity;
+import com.example.fragment.slideActivity.SlideMainActivity;
 import com.example.fragment.tabhost.FragmentTabHostDemo;
 import com.example.fragment.viewpager.ViewPagerActivity;
 
@@ -21,6 +22,7 @@ public class ActivityMainActivity extends Activity implements View.OnClickListen
         findViewById(R.id.button_slidemenu).setOnClickListener(this);
         findViewById(R.id.button_vp_host).setOnClickListener(this);
         findViewById(R.id.button_view_pager).setOnClickListener(this);
+        findViewById(R.id.button_slide_main).setOnClickListener(this);
     }
 
     @Override
@@ -41,6 +43,12 @@ public class ActivityMainActivity extends Activity implements View.OnClickListen
                 startActivity(intent);
             }
             break;
+            case R.id.button_slide_main: {
+                Intent intent = new Intent(this, SlideMainActivity.class);
+                startActivity(intent);
+            }
+            break;
+
             case R.id.button_slidemenu:
                 break;
         }
---
+++ app/src/main/java/com/example/fragment/slideActivity/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.fragment.slideActivity;
+
+import android.os.Bundle;
+import com.example.fragment.R;
+
+/**
+ * Created by chenjishi on 14-3-17.
+ */
+public class DetailActivity extends SlidingActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
